e243e0ad1220fa095868836b0c92cfa7dd92b3b0
DanteLuque <danteluque706@gmail.com>
2025-05-17 22:27:42 -0500

    feat: 🎸 projet-base

---
+++ src/server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import db from './config/mysql/mysql.js';
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+
+        this.connectDB();
+        this.middlewares();
+        this.routes();
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log(`👾 I'M ALIVE => PORT: ${this.port}`);
+        });
+    }
+
+    async connectDB() {
+        await db.testConnection();
+    }
+
+    middlewares() {
+        this.app.use(express.json());
+    }
+
+    routes() {
+    }
+}
+
+export default Server;
